bc7e387555c9a72084adc64840b5f6ae7916c982
Benoit-Dphn <dauphin.benoit.fr@gmail.com>
2026-04-23 15:26:11 +0200

    add fct and implemented parse board and get pion pos

--- utile.py
+++ utile.py
@@ -1,3 +1,28 @@
-from inscription import handle_server_requests as hsr
+board = []
+player = 0
+color = ""
 
 def parse_board(state) :
+    board = state["board"]
+    player = state["current"]
+    color = state["color"]
+
+    return board, player, color
+
+def get_pion_position(board,player, color):
+    if player == 0:
+        player = "light"
+    else : player = "dark"
+    pos = board[[1],[color, player]]
+    
+def get_legal_moves(board, player, color):
+    pass
+
+def is_move_legal(board, player, from_pos, to_pos):
+    pass
+
+def get_next_color(board, to_pos):
+    pass
+
+def is_winning(board, player):
+    pass
